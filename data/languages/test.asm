d1527e1b80c1107294b6497d40b9101654e7a183
mwdmwd <piotr@wojnarow.ski>
2024-05-10 01:56:57 +0200

    stuff

--- data/languages/test.asm
+++ data/languages/test.asm
@@ -1,1 +1,20 @@
-aborti
+	.text
+	.newblock
+
+	.asmfunc
+	ABORTI
+	ABS          ACC
+	ABSTC        ACC
+	ADD          ACC, #0x1234 << 10
+
+	ADD          ACC, PL << T
+
+	ADD          ACC, PL
+	MOV          ACC, PL
+	ADD          ACC, AR1 << 9
+
+	ADD          AL, @0x21
+	ADD          AH, @0x21
+	ADD          @0x21, AL
+	ADD          @0x21, AH
+	.endasmfunc
--- data/languages/test.py
+++ data/languages/test.py
@@ -3,6 +3,19 @@ from pypcode import Context, PcodePrettyPrinter
 
 ctx = Context("tms320c28:LE:32:default")
 
-dx = ctx.disassemble(b"\x01\x00\x56\xff\x5f\x56\x1a\xff\x34\x12\x23\x56\xab\x00\xab\x81\xab\x85\x04\x56\xa1\x09\x21\x94\x21\x95\x21\x72\x21\x73")
+t = b"\x01\x00\x56\xff\x5f\x56\x1a\xff\x34\x12\x23\x56\xab\x00\xab\x81\xab\x85\x04\x56\xa1\x09\x21\x94\x21\x95\x21\x72\x21\x73"
+with open("test.bin", "wb") as f:
+    f.write(t)
+
+dx = ctx.disassemble(t)
 for ins in dx.instructions:
-    print(f"{ins.addr.offset:#x}/{ins.length}: {ins.mnem} {ins.body}")
+    ib = bytearray(t[ins.addr.offset : ins.addr.offset + ins.length])
+    for i in range(0, len(ib), 2):
+        ib[i], ib[i + 1] = ib[i + 1], ib[i]
+
+    words = ib.hex(" ", 2).split()
+    #print(f"{ins.addr.offset:#x}/{ins.length}: {words[0]} {ins.mnem} {ins.body}")
+    wordOffset = ins.addr.offset // 2
+    print(f"{wordOffset:08x}   {words[0]}   {ins.mnem.upper():13}{ins.body}")
+    for i, word in enumerate(words[1:], start=1):
+        print(f"{wordOffset + i:08x}   {word}")
