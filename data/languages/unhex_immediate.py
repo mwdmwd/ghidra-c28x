62351e305abc16ae22adc581ad9b052c58f6ee6a
mwdmwd <piotr@wojnarow.ski>
2024-05-17 03:40:11 +0200

    unhex all immediates except after @

--- data/languages/unhex_immediate.py
+++ data/languages/unhex_immediate.py
@@ -2,11 +2,9 @@
 import sys
 import re
 
-
-HEX_IMMEDIATE = re.compile(r"#(0x[0-9a-fA-F]+)")
-HEX_IMMEDIATE_COMMA = re.compile(r", (0x[0-9a-fA-F]+)")
+# Everything except after @ signs
+HEX_IMMEDIATE_ALL = re.compile(r"(?<!@)0x[0-9a-fA-F]+")
 
 for line in sys.stdin:
-    line = HEX_IMMEDIATE.sub(lambda hi: "#" + str(int(hi.group()[1:], 16)), line)
-    line = HEX_IMMEDIATE_COMMA.sub(lambda hi: ", " + str(int(hi.group()[2:], 16)), line)
+    line = HEX_IMMEDIATE_ALL.sub(lambda hi: str(int(hi.group(), 16)), line)
     print(line, end="")
